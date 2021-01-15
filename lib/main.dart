e9276b7e2f41776efca078ebd862a13e44c988ef
Arunoday kumar <201951031@iiitvadodara.ac.in>
2021-01-15 16:16:42 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+void main(){
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlueAccent,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue,
+        ),
+        body: AskAnything(),
+      ),
+    ),
+  );
+}
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+class AskAnything extends StatefulWidget {
+  @override
+  _AskAnythingState createState() => _AskAnythingState();
+}
+
+class _AskAnythingState extends State<AskAnything> {
+  int num = 3;
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                setState(() {
+                  num = Random().nextInt(5)+1;
+                });
+              },
+              child: Image.asset('images/ball$num.png'),
+            ),
+          )
+        ],
       ),
     );
+  }
+}
+
